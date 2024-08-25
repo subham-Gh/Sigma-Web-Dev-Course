aec70ba4710dd4f4523186d25a219c1b8549bd07
subham-Gh <155185819+subham-Gh@users.noreply.github.com>
2024-08-25 22:37:01 +0530

    Lec 54 Completed

---
+++ Lec 54/script.js
@@ -0,0 +1,18 @@
+// alert("Hello World");
+
+// console.log("hello world");
+
+// var a = prompt("Enter your number");
+// console.log("Your number is: ", a);
+
+var isTrue = confirm("Are you sure yoy wnat to blast your computer?");
+
+if (isTrue) {
+  console.log("Your computer is blasting");
+} else {
+  console.log("Your computer is not blasting");
+}
+
+document.title = "Hey I am good";
+
+document.body.style.backgroundColor = "red";
